39cb19d4aecb289ef14621199e8ca95c3b99c8d3
LuxunVSHushi <kore2009@163.com>
2018-05-08 19:56:33 +0800

    2018.5.8 changes

--- act1/1.23.cpp
+++ act1/1.23.cpp
@@ -1,4 +1,14 @@
+
+/*
+
+本程序符合现实中的输入习惯。随便输入一本书的ISBN，能够做到自动存储，最后遍历搜索
+一个存储数据的数组，然后求出某一个ISBN共卖出去多少本。。 
+
+*/
+
+
 #include<iostream>
+
 #include"Sales_item.h"
 
 using namespace std;
@@ -7,6 +17,8 @@ int main ()
 {  
 	Sales_item book1, book[100]={};
 int i=0,j=0;
+cin>>book1;
+book[0]=book1;
 while((cin>>book1)&&(book1.isbn()!="qq"))//输入qq退出输入 
 	{
 		for( j=0;j<=i;++j)
@@ -16,7 +28,7 @@ while((cin>>book1)&&(book1.isbn()!="qq"))//输入qq退出输入
 				 } 
 			
 		}
-		if(j==i)
+		if(j==i+1)
 		{i++;
 		book[i]=book1;
 		j=0;
@@ -26,6 +38,6 @@ while((cin>>book1)&&(book1.isbn()!="qq"))//输入qq退出输入
 	}
 	
 	for(j=0;j<=i;j++)
-	cout<<book[j];
+	cout<<book[j]<<endl;
 	return 0;
 }
